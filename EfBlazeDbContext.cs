25feb6e721b8da020c83f4feb4105dca68130a4f
Erik Johnson <akwaduk13@gmail.com>
2023-12-14 20:35:59 -0600

    Initial commit

---
+++ EfBlazeDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFBlaze;
+
+public class EfBlazeDbContext : DbContext
+{
+
+}
---
+++ EfBlazeIdentityDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace EFBlaze;
+
+public class EfBlazeIdentityDbContext<TUser> : IdentityDbContext<TUser> 
+    where TUser : IdentityUser
+{
+    public EfBlazeIdentityDbContext(DbContextOptions<EfBlazeIdentityDbContext<TUser>> options) 
+        : base(options) 
+    {
+    }
+
+    // Other DbContext code here
+}
